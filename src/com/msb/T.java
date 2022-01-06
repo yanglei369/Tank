35f86f13ff1cc62ecb47c0a294aca489d3185491
yl <906402705@qq.com>
2022-01-06 09:54:15 +0800

    Tank第二次尝试提交代码到Git。

---
+++ src/com/msb/T.java
@@ -0,0 +1,18 @@
+package com.msb;
+
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+/**
+ * @Author: yl
+ * @Date: 2022/1/6 9:46
+ * @Description: com.msb
+ * @Version: 1.0
+ */
+public class T {
+    //main
+    public static void main(String[] args) {
+        TankFrame tf = new TankFrame();
+    }
+}
--- src/com/msb/TankFrame.java
+++ src/com/msb/TankFrame.java
@@ -10,21 +10,18 @@ import java.awt.event.WindowEvent;
  * @Description: com.msb
  * @Version: 1.0
  */
-public class TankFrame {
-    //main
-    public static void main(String[] args) {
-        Frame f = new Frame();
-        f.setSize(800,600);
-        f.setResizable(false);
-        f.setTitle("Tank War");
-        f.setVisible(true);
+public class TankFrame extends Frame{
+    public TankFrame() throws HeadlessException {
+        setSize(800,600);
+        setResizable(false);
+        setTitle("Tank War");
+        setVisible(true);
         //窗口可关闭
-        f.addWindowListener(new WindowAdapter() {  //匿名内部类  WindowAdapter实现了WindowListener的接口
+        addWindowListener(new WindowAdapter() {  //匿名内部类  WindowAdapter实现了WindowListener的接口
             @Override
             public void windowClosing(WindowEvent e) {
                 System.exit(0);
             }
         });
     }
-
 }
